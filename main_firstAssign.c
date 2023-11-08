64ea723a26d04b3e115e1d2f543fd55f6541acf8
Gianluca Galvagni <gianluca.galvagni99@gmail.com>
2023-11-08 12:49:37 +0100

    Update main_firstAssign.c

--- main_firstAssign.c
+++ main_firstAssign.c
@@ -49,7 +49,8 @@ void printFunctionFirstRow(char receivedChar);
 int number_readings = 0; // global variable for counting all the printed value
 int number_first_raw = 0; // global variable for counting the printed value on the first raw
 int position_first_raw[16] = {0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8A,0x8B,0x8C,0x8D,0x8E,0x8F};
-int control_interrupt1 = 0; // interrupt flag
+int flag_S5 = 0; // interrupt flag S5
+int flag_S6 = 0; // interrupt flag S6
 /*
 int bufferLength = 0; // writeIndex - readIndex
 int readIndex = 0;
@@ -249,20 +250,13 @@ void __attribute__((__interrupt__, __auto_psv__)) _U2RXInterrupt(void) {
 void __attribute__((__interrupt__, __auto_psv__)) _INT0Interrupt
     (){
     IFS0bits.INT0IF = 0; // reset interrupt flag
-   
-    while (U2STAbits.UTXBF);  // Wait for the transmit buffer to be empty
-    U2TXREG = number_readings;  // Send the number of chars received
+    flag_S5 = 1;
 }
 
 void __attribute__((__interrupt__, __auto_psv__)) _INT1Interrupt
     (){
     IFS1bits.INT1IF = 0; // reset interrupt flag
-   
-    cleaningFirstRow();
-    number_readings = 0;
-    number_first_raw = 0;
-    convertNumberToString(number_readings); 
-    setCursorPositionFirstROw(0x80);
+    flag_S6 = 1; 
 }
 
 // CIRCULAR BUFFER
@@ -312,6 +306,24 @@ char pull(){
     }
 }
 
+// INTERUPT FLAGS
+void checkFlagsInterrupt(){
+    if(flag_S5 == 1){
+        while (U2STAbits.UTXBF);  // Wait for the transmit buffer to be empty
+        U2TXREG = number_readings;  // Send the number of chars received
+        flag_S5 = 0;
+        }
+    if(flag_S6 == 1){
+        cleaningFirstRow();
+        number_readings = 0;
+        number_first_raw = 0;
+        convertNumberToString(number_readings); 
+        setCursorPositionFirstROw(0x80);
+        flag_S6 = 0;
+    }
+    return;
+}
+
 int main(void) {
     IEC1bits.U2RXIE = 1; // Abilita l'interrupt per la ricezione UART2
     U2STAbits.URXISEL = 3; // Set URXISEL to 11
@@ -341,6 +353,8 @@ int main(void) {
             receivedChar = pull();
             printFunctionFirstRow(receivedChar);
         }
+        // check the interrupts flag
+        checkFlagsInterrupt();
 
         IFS0bits.T1IF = 0; // Reset the flag
         T1CONbits.TON = 1; // Starts the timer
